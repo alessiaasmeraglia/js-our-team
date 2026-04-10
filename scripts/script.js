e57223994f36cf09a149980cb98a9c91b47c7bcc
Alessia Smeraglia <alessia.smeraglia@gmail.com>
2026-04-10 16:26:55 +0200

    Aggiunti membri e stile

---
+++ scripts/script.js
@@ -0,0 +1,36 @@
+'use strict';
+
+//Seleziono gli elementi del DOM
+const teamContainer = document.getElementById('team-container');
+
+//Funzione per creare le card dei membri del team
+function createTeamCards(array) {
+    
+    for( let i = 0; i < array.length; i++) {
+        const member = array[i];
+        const card = document.createElement('div');
+        card.classList.add('member');
+
+        const name = document.createElement('h3');
+        name.textContent = member.name;
+        card.appendChild(name);
+
+        const role = document.createElement('p');
+        role.textContent = member.role;
+        card.appendChild(role);
+
+        const email = document.createElement('p');
+        email.textContent = member.email;
+        card.appendChild(email);
+
+        const img = document.createElement('img');
+        img.src = member.img;
+        card.appendChild(img);
+
+        teamContainer.appendChild(card);
+
+    }
+}
+
+//Chiamo la funzione per creare le card dei membri del team
+createTeamCards(teamMembers);
