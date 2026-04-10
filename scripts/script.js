f6349a64dfeb1c9a80001a15d728efbf4ce62954
Alessia Smeraglia <alessia.smeraglia@gmail.com>
2026-04-10 17:10:34 +0200

    spostato gli appendChild

--- scripts/script.js
+++ scripts/script.js
@@ -6,27 +6,39 @@ const teamContainer = document.getElementById('team-container');
 //Funzione per creare le card dei membri del team
 function createTeamCards(array) {
     
-    for(const member of array) {
+    for(let i = 0; i < array.length; i++) {
+        const member = array[i];
+
         const card = document.createElement('div');
         card.classList.add('member');
 
+        const img = document.createElement('img');
+        img.src = member.img;
+
+        const infoContainer = document.createElement('div');
+        infoContainer.classList.add('member-info');
+        
+
         const name = document.createElement('h3');
         name.textContent = member.name;
-        card.appendChild(name);
+        
 
         const role = document.createElement('p');
         role.textContent = member.role;
-        card.appendChild(role);
+        
 
         const email = document.createElement('p');
         email.textContent = member.email;
-        card.appendChild(email);
+        email.classList.add('email');
 
-        const img = document.createElement('img');
-        img.src = member.img;
-        card.appendChild(img);
 
         teamContainer.appendChild(card);
+        infoContainer.appendChild(name);
+        infoContainer.appendChild(role);
+        infoContainer.appendChild(email);
+
+        card.appendChild(img);
+        card.appendChild(infoContainer);
 
     }
 }
